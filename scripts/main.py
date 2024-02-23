b186de7cfdef2c388b8b3c3f8ae7ea1100788e84
xhayper <46065349+xhayper@users.noreply.github.com>
2024-02-23 10:09:19 +0700

    feat: update the script

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,6 @@
+import datetime
 import requests
+import json
 import bs4
 import os
 
@@ -8,18 +10,51 @@ ASTRODX_TESTFLIGHTS = {
     "Group C": "https://testflight.apple.com/join/CuMxZE2M",
 }
 
-def main():
-    statuses = {}
-    
-    for name, link in ASTRODX_TESTFLIGHTS.items():
-        page = requests.get(
-            link,
-            headers={
-                "Accept-Language": "en-us"
-            }
-        ).text
 
-        soup = bs4.BeautifulSoup(page, 'html.parser')
+def readData() -> dict:
+    try:
+        with open("data.json", "r") as openfile:
+            return json.load(openfile)
+    except FileNotFoundError:
+        return {}
+
+
+def writeData(data: dict):
+    with open("data.json", "w+") as file:
+        json.dump(data, file, indent=4)
+
+
+data = readData()
+
+
+def constructMessage(status: str, name: str, link: str) -> dict:
+    upperCaseFirstLetter = lambda s: s[0].upper() + s[1:]
+    return {
+        "embeds": [
+            {
+                "title": f"TestFlight {name} status",
+                "color": 65280 if status == "open" else 16711680,
+                "fields": [
+                    {
+                        "name": "Status",
+                        "value": upperCaseFirstLetter(status),
+                        "inline": True,
+                    },
+                    {"name": "Link", "value": link, "inline": True},
+                ],
+                "timestamp": datetime.datetime.now().isoformat(),
+            }
+        ]
+    }
+
+
+def getTestFlightStatuses() -> dict[str, str]:
+    statuses = {}
+
+    for name, link in ASTRODX_TESTFLIGHTS.items():
+        page = requests.get(link, headers={"Accept-Language": "en-us"}).text
+
+        soup = bs4.BeautifulSoup(page, "html.parser")
         status_text = soup.select(".beta-status span")[0].get_text()
         if "This beta is full." in status_text:
             status = "full"
@@ -30,47 +65,45 @@ def main():
 
         statuses[name] = status
 
-    anyOpen = "open" in statuses.values()
+    return statuses
 
-    if not anyOpen:
+
+def main():
+    discordWebhookUrl = os.environ.get("DISCORD_WEBHOOK_URL")
+
+    if discordWebhookUrl is None:
+        print("DISCORD_WEBHOOK_URL environment variable not set")
         return
 
-    content = ""
-    color = 16711680
-
-    if anyOpen:
-        # content = "<@&1190732170126442557>"
-        color = 65280
-    
-    webhookUrl = os.environ["DISCORD_WEBHOOK_URL"]
-
-    description = []
+    statuses = getTestFlightStatuses()
 
     for name, status in statuses.items():
-        description.append(name + ": " + status)
+        if not "messageId" in data:
+            data["messageId"] = {}
 
-    description = "\n".join(description)
+        editMessage = False
 
-    fields = []
+        if name in data["messageId"]:
+            editMessage = True
 
-    for name, link in ASTRODX_TESTFLIGHTS.items():
-        fields.append({
-            "name": name,
-            "value": link,
-            "inline": True
-        })
+        embed = constructMessage(status, name, ASTRODX_TESTFLIGHTS[name])
 
-    requests.post(webhookUrl, json={
-        "content": content,
-        "embeds": [
-            {
-                "title": "TestFlight status",
-                "color": color,
-                "description": description,
-                "fields": fields
-            }
-        ]
-    })
+        if editMessage:
+            requests.patch(
+                f"{discordWebhookUrl}/messages/{data['messageId'][name]}", json=embed
+            )
+        else:
+            response = requests.post(
+                f"{discordWebhookUrl}?wait=true",
+                headers={
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                },
+                json=embed,
+            )
+            data["messageId"][name] = response.json()["id"]
+
+    writeData(data)
 
 if __name__ == "__main__":
     main()
